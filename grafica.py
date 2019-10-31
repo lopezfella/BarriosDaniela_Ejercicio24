8d38837429e5ff0a20e57eb3d1ef893df6c4d455
lopezfella <56890855+lopezfella@users.noreply.github.com>
2019-10-31 13:46:10 -0500

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -1,6 +1,11 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 plt.figure()
 data = np.loadtxt("datos.dat")
-plt.plot(data[:,0], data[:,1], label = 'Funcion gaussiana')
-plt.hist(v, bins=25, color='g', label='Números que siguen una distribución gaussiana')
+plt.hist(data, bins=25, color='g', label = 'gaussiana')
 plt.legend()
+plt.xlabel('X')
+plt.ylabel('Histograma')
 plt.savefig("gausiana.png")
